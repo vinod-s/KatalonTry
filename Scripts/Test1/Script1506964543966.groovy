f05c136ca9a97fd2a2e222bf92a26b314954ca69
vsarpabhus001 <vsarpabhus001@VSARPABHUS00160.us.nam.ad.pwcinternal.com>
2017-11-09 18:48:44 +0530

    paytm

--- Scripts/Test1/Script1506964543966.groovy
+++ Scripts/Test1/Script1506964543966.groovy
@@ -19,7 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser('www.google.com')
+WebUI.openBrowser('www.paytm.com')
 
 WebUI.closeBrowser()
 
