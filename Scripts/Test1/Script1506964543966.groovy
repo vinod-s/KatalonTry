e97388ad03fd6925db1de60bb348fd1dbf9f7b35
vinod-s <vinu.s.b@gmail.com>
2017-11-09 19:00:59 +0530

    Update Script1506964543966.groovy

--- Scripts/Test1/Script1506964543966.groovy
+++ Scripts/Test1/Script1506964543966.groovy
@@ -19,5 +19,5 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser('www.google.com')
+WebUI.openBrowser('www.google.co.in')
 
